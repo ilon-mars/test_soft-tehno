05a630510e0f3515e9581c46c8e332d371e3a0b3
Maryana <ilon.mars@yandex.ru>
2021-10-05 00:02:51 +0300

    delete: src/

--- src/js/modules/fetchData.js
+++
@@ -1,9 +0,0 @@
-import sendRequest from './sendRequest';
-
-async function fetchData() {
-  const requestURL = './api/updates.json';
-  let data = await sendRequest('GET', requestURL);
-  return data;
-}
-
-export default fetchData;
